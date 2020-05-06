be597f2ee7d61e7a4ae3a81031c6b7214c63fdd6
AntonKuksov <Anton.Kuksov@github.com>
2020-05-06 20:36:40 +0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus maxime perspiciatis ullam eos. Expedita iusto laborum animi error natus labore ipsa quia rerum cum exercitationem. Libero ipsum perferendis commodi dolore?</p>
+
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
